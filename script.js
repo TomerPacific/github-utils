94a83c8d79be3d1b30fb73a017ebcfa340ded489
unknown <tomerpacific@gmail.com>
2019-12-25 18:53:03 +0200

    github-utils logic to fetch user's repositories

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+
+const GITHUB_REPOSITORIES_URL = "https://api.github.com/users/"
+const GET_REQUEST = "GET";
+const READY_STATE_OK = 4;
+const RESPONSE_STATUS_OK = 200;
+let request = null;
 let usernameInput = document.getElementById('username_input');
 
 function fetchUserRepositories() {
     let username = usernameInput.value;
-    
+    request = new XMLHttpRequest();
+    let url = GITHUB_REPOSITORIES_URL + username + '/repos?per_page=100';
+    request.open(GET_REQUEST, url);
+    request.send();
+
+    request.onreadystatechange = function() {
+        if (this.readyState === READY_STATE_OK && this.status === RESPONSE_STATUS_OK) {
+            
+        }
+    }
 }

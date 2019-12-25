a7200d56aabe2bfd3368257cc7bc77f6ce7f79fe
unknown <tomerpacific@gmail.com>
2019-12-25 19:00:31 +0200

    github-utils logic to parse repositories and show them to the user

--- script.js
+++ script.js
@@ -4,18 +4,31 @@ const GET_REQUEST = "GET";
 const READY_STATE_OK = 4;
 const RESPONSE_STATUS_OK = 200;
 let request = null;
+
 let usernameInput = document.getElementById('username_input');
+let repositoriesList = document.getElementById('repositories');
+
+//name
+//html_url
+//description
+//stargazers_url
 
 function fetchUserRepositories() {
     let username = usernameInput.value;
     request = new XMLHttpRequest();
     let url = GITHUB_REPOSITORIES_URL + username + '/repos?per_page=100';
     request.open(GET_REQUEST, url);
-    request.send();
+    request.send(null);
 
     request.onreadystatechange = function() {
         if (this.readyState === READY_STATE_OK && this.status === RESPONSE_STATUS_OK) {
-            
+            let repositories = JSON.parse(this.responseText);
+            for (let index = 0; index < repositories.length; index++) {
+                let repository = repositories[index];
+                let liElement = document.createElement('li');
+                liElement.innerHTML = repository.name + ' | ' + repository.description;
+                repositoriesList.appendChild(liElement);
+            }
         }
     }
 }

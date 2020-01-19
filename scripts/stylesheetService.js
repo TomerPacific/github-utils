d1df20cd2b38de7c71a1e4bf551e9a9f285f275f
unknown <tomerpacific@gmail.com>
2020-01-19 13:14:33 +0200

    github-utils removing logs and patching logic

--- scripts/stylesheetService.js
+++ scripts/stylesheetService.js
@@ -1,16 +1,12 @@
 const CODING_LANGUAGE_STYLESHEET = document.styleSheets[4];
 
-function hasCssRule(cssRule) {
+function hasCssRule(cssRuleToFind) {
     for(let index = 0; index < CODING_LANGUAGE_STYLESHEET.cssRules.length; index++) {
         let cssRule = CODING_LANGUAGE_STYLESHEET.cssRules[index];
-        console.log(cssRule.selectorText);
-        console.log(cssRule);
-        console.log(cssRule.selectorText.substring(1));
-        if (cssRule.selectorText.substring(1) === cssRule) {
-            console.log('Returning true for ' + cssRule);
+        if (cssRule.selectorText === cssRuleToFind) {
             return true;
         }
     }
-    console.log('Returning false for ' + cssRule);
+
     return false;
 }

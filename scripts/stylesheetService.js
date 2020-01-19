3b6d904922fbf8982de58f13b83185bfeaf6fea8
unknown <tomerpacific@gmail.com>
2020-01-19 13:16:06 +0200

    github-utils shortening rule to match

--- scripts/stylesheetService.js
+++ scripts/stylesheetService.js
@@ -3,7 +3,7 @@ const CODING_LANGUAGE_STYLESHEET = document.styleSheets[4];
 function hasCssRule(cssRuleToFind) {
     for(let index = 0; index < CODING_LANGUAGE_STYLESHEET.cssRules.length; index++) {
         let cssRule = CODING_LANGUAGE_STYLESHEET.cssRules[index];
-        if (cssRule.selectorText === cssRuleToFind) {
+        if (cssRule.selectorText.substring(1) === cssRuleToFind) {
             return true;
         }
     }

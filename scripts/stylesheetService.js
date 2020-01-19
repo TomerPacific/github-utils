ae7b04bc138c4de7d9c6e68aee10a0b6fce03379
unknown <tomerpacific@gmail.com>
2020-01-19 13:11:48 +0200

    github-utils logs to find out where problem is

--- scripts/stylesheetService.js
+++ scripts/stylesheetService.js
@@ -3,6 +3,9 @@ const CODING_LANGUAGE_STYLESHEET = document.styleSheets[4];
 function hasCssRule(cssRule) {
     for(let index = 0; index < CODING_LANGUAGE_STYLESHEET.cssRules.length; index++) {
         let cssRule = CODING_LANGUAGE_STYLESHEET.cssRules[index];
+        console.log(cssRule.selectorText);
+        console.log(cssRule);
+        console.log(cssRule.selectorText.substring(1));
         if (cssRule.selectorText.substring(1) === cssRule) {
             console.log('Returning true for ' + cssRule);
             return true;
